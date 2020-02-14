c9edc7c3a5589e0eec10f0f56dd5594dadae3498
JonasBeal <jonas.beal@curie.fr>
2020-02-14 18:36:59 +0100

    Rearrage files

---
+++ BRAF_CL.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+list1="Proliferation_b1,Proliferation_b2"
+
+#Please first cd to the parent folder
+#With mut
+python3 Scripts/Simulations/MaBoSS_specific_drugs_py.py BRAF_Model -sy Mac -p 2 "BRAF_Model_CL_mut.txt" -o $list1 -s "mut" -m "Results/Profiles/BRAF_Model_CL_mut.csv" -d "Results/Profiles/BRAF_Model_drug_profiles.csv"
+
+#RNA alone
+python3 Scripts/Simulations/MaBoSS_specific_drugs_py.py BRAF_Model -sy Mac -p 2 "BRAF_Model_CL_RNA.txt" -o $list1 -s "RNA" -rb "Results/Profiles/BRAF_Model_CL_RNA_norm.csv" -rf 100 -d "Results/Profiles/BRAF_Model_drug_profiles.csv"
+
+#All
+python3 Scripts/Simulations/MaBoSS_specific_drugs_py.py BRAF_Model -sy Mac -p 2 "BRAF_Model_CL_mut_RNA.txt" -o $list1 -s "mut_RNA" -m "Results/Profiles/BRAF_Model_CL_mut.csv" -rb "Results/Profiles/BRAF_Model_CL_RNA_norm.csv" -rf 100 -d "Results/Profiles/BRAF_Model_drug_profiles.csv"
+
+
+
+
+
