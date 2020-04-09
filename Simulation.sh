de5d2ae98d89c8264e827653933ba472d8af0208
JonasBeal <jonas.beal@curie.fr>
2020-04-09 17:37:16 +0200

    New model and improved tests

--- Simulation.sh
+++ Simulation.sh
@@ -2,14 +2,17 @@
 list1="Proliferation_b1,Proliferation_b2"
 
 #Please first cd to the parent folder
+
+model="BRAF_Model"
+
 #With mut
-python3 Scripts/Simulations/MaBoSS_PROFILE_drugs.py BRAF_Model -sy Mac -p 2 "BRAF_Model_CL_mut.txt" -o $list1 -s "mut" -m "Results/Profiles/BRAF_Model_CL_mut.csv" -d "Results/Profiles/BRAF_Model_drug_profiles.csv"
+python3 Scripts/Simulations/MaBoSS_PROFILE_drugs.py $model -sy Mac -p 2 $model"_CL_mut.txt" -o $list1 -s "mut" -m "Results/Profiles/"$model"_CL_mut.csv" -d "Results/Profiles/BRAF_Model_drug_profiles.csv"
 
 #RNA alone
-python3 Scripts/Simulations/MaBoSS_PROFILE_drugs.py BRAF_Model -sy Mac -p 2 "BRAF_Model_CL_RNA.txt" -o $list1 -s "RNA" -rb "Results/Profiles/BRAF_Model_CL_RNA_norm.csv" -rf 100 -d "Results/Profiles/BRAF_Model_drug_profiles.csv"
+python3 Scripts/Simulations/MaBoSS_PROFILE_drugs.py $model -sy Mac -p 2 $model"_CL_RNA.txt" -o $list1 -s "RNA" -rb "Results/Profiles/"$model"_CL_RNA_norm.csv" -rf 100 -d "Results/Profiles/BRAF_Model_drug_profiles.csv"
 
 #All
-python3 Scripts/Simulations/MaBoSS_PROFILE_drugs.py BRAF_Model -sy Mac -p 2 "BRAF_Model_CL_mut_RNA.txt" -o $list1 -s "mut_RNA" -m "Results/Profiles/BRAF_Model_CL_mut.csv" -rb "Results/Profiles/BRAF_Model_CL_RNA_norm.csv" -rf 100 -d "Results/Profiles/BRAF_Model_drug_profiles.csv"
+python3 Scripts/Simulations/MaBoSS_PROFILE_drugs.py $model -sy Mac -p 2 $model"_CL_mut_RNA.txt" -o $list1 -s "mut_RNA" -m "Results/Profiles/"$model"_CL_mut.csv" -rb "Results/Profiles/"$model"_CL_RNA_norm.csv" -rf 100 -d "Results/Profiles/BRAF_Model_drug_profiles.csv"
 
 
 
--- Test_WT.sh
+++ Test_WT.sh
@@ -2,9 +2,13 @@
 list1="Proliferation_b1,Proliferation_b2"
 
 #Please first cd to the parent folder
-python3 Scripts/Simulations/MaBoSS_PROFILE_drugs.py BRAF_Model -sy Mac -p 2 "BRAF_Model_CL_n5000_t100.txt" -o $list1 -s "n5000_t100" 
+python3 Scripts/Simulations/MaBoSS_PROFILE_drugs.py BRAF_Model -sy Mac -p 2 "Tests/BRAF_Model_CL_n5000_t100.txt" -o $list1 -s "n5000_t50" -rb "Results/Profiles/BRAF_Model_Test.csv" -rf 100
 
 
+#for i in {1..100}
+#do
+#   python3 Scripts/Simulations/MaBoSS_PROFILE_drugs.py BRAF_Model -sy Mac -p 1 "Tests/BRAF_Model_CL_n5000_t50_"$i".txt" -o $list1 -s "n5000_t50" 
+#done
 
 
 
